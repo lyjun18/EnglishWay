81248d238c980455bc204a270266cfb6e32b4156
lyjun <lyjun18@gmail.com>
2021-07-26 17:17:52 +0800

    7/26: PetActivity改為PetsActivity,作為寵物一覽頁面 新增PetActivity,作為個別寵物頁面 新增FeedActivity,作為寵物餵食頁面

--- app/src/main/java/com/example/englishway/PetActivity.java
+++ app/src/main/java/com/example/englishway/PetActivity.java
@@ -2,9 +2,12 @@ package com.example.englishway;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class PetActivity extends AppCompatActivity {
 
@@ -14,24 +17,46 @@ public class PetActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pet);
         //取得控制項物件
         initViews();
+        //顯示寵物
+        showResults();
         //設定監聽事件
-        setListensers();
-    }
-    //private Button button_back;
-
-    private void initViews(){
-
-        //button_back = (Button)findViewById(R.id.button_back);
-    }
-    private void setListensers(){
-
-        //button_back.setOnClickListener(backtoMain);
+        setListeners();
     }
 
-    private View.OnClickListener backtoMain = new View.OnClickListener() {
+    private TextView pet_name;
+    private Button button_feed;
+    private Button button_evolve;
+
+    private void initViews() {
+        pet_name = findViewById(R.id.textView);
+        button_feed = findViewById(R.id.button_feed);
+        button_evolve = findViewById(R.id.button_evolve);
+    }
+
+    private void setListeners() {
+        button_feed.setOnClickListener(feedPet);
+    }
+
+    private void showResults() {
+        try {
+            Bundle bundle = this.getIntent().getExtras();
+            String petID = bundle.getString("ONCLICK_PET");
+            pet_name.setText(petID);
+        } catch (Exception obj) {
+            Toast.makeText(this, "錯誤", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    private View.OnClickListener feedPet = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            PetActivity.this.finish();
+            Intent intent = new Intent();
+            Bundle bundle = new Bundle();
+            bundle.putString("PET_NAME",pet_name.getText().toString());
+            intent.setClass(PetActivity.this, FeedActivity.class);
+            intent.putExtras(bundle);
+            startActivity(intent);
         }
     };
 
