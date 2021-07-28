59474d0da709082f27ca8fed1fb8cf9e86732dcf
lyjun <lyjun18@gmail.com>
2021-07-28 18:10:16 +0800

    7/26: 更新FeedActivity

--- app/src/main/java/com/example/englishway/FeedActivity.java
+++ app/src/main/java/com/example/englishway/FeedActivity.java
@@ -3,6 +3,8 @@ package com.example.englishway;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,13 +21,22 @@ public class FeedActivity extends AppCompatActivity {
         //設定監聽事件
         setListeners();
     }
+
     private TextView pet_name;
+    private Button button_1;
+    private Button button_2;
 
     private void initViews() {
+
         pet_name = findViewById(R.id.textView);
+        button_1 = findViewById(R.id.button1);
+        button_2 = findViewById(R.id.button2);
     }
 
-    private void setListeners() {}
+    private void setListeners() {
+        button_1.setOnClickListener(itemOnclick);
+        button_2.setOnClickListener(itemOnclick);
+    }
 
     private void showResults() {
         try {
@@ -36,4 +47,20 @@ public class FeedActivity extends AppCompatActivity {
             Toast.makeText(this, "錯誤", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private View.OnClickListener itemOnclick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String item_name = "";
+            switch (v.getId()) {
+                case R.id.button1:
+                    item_name = "道具1";
+                    break;
+                case R.id.button2:
+                    item_name = "道具2";
+                    break;
+            }
+            Toast.makeText(FeedActivity.this,item_name,Toast.LENGTH_SHORT).show();
+        }
+    };
 }
